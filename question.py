62906bd1c6735b43c1f2fb8b76bc88d98056e5d9
Bukarinovv <bukarinovivan@yandex.ru>
2024-06-18 17:05:47 +0300

    4 and 5

--- question.py
+++ question.py
@@ -1,12 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import calendar, locale
-from datetime import datetime
 
 # 1 Вопрос
 df = pd.read_excel('data.xlsx', usecols="B, C", header=0, skiprows=list(range(1, 260)), skipfooter=731-370)
 filtered_df = df[df['status'] != 'ПРОСРОЧЕНО']
-#print(f'Первый вопрос: {round(filtered_df['sum'].sum(), 2)} руб.')
+print(f'Первый вопрос: {round(filtered_df['sum'].sum(), 2)} руб.')
 
 
 # 2 Вопрос
@@ -24,24 +23,25 @@ months = list(map(lambda x: calendar.month_name[x], data.keys()))
 plt.plot(months, data.values(), c = 'r')
 plt.grid(True)
 plt.title('Выручка по месяцам, руб.')
-#plt.show()
-
+plt.show()
 
 
 # 3 Вопрос
 df = pd.read_excel('data.xlsx', usecols="B, D", header=0, skiprows=list(range(1, 486)), skipfooter=731-595)
 group = df.groupby('sale').sum()
 sorted_group = group.sort_values(by='sum', ascending=False)
-#print(f'Третий вопрос: {sorted_group.iloc[0].name}')
+print(f'Третий вопрос: {sorted_group.iloc[0].name}')
 
 
 #4 Вопрос
 df = pd.read_excel('data.xlsx', usecols='E', header=0, skiprows=list(range(1,596)))
 group = df['new/current'].value_counts(ascending=False).index.tolist()
-#print(f'Четвертый вопрос: {group[0]}')
+print(f'Четвертый вопрос: {group[0]}')
 
 
 #5 Вопрос
 df = pd.read_excel('data.xlsx', usecols='G, H', header=0, skiprows=[1, 2])
 filtered_df = [str(x) for x in df['receiving_date'] if x.month == 6]
-print(len(filtered_df))
+print(f'Пятый вопрос: {len(filtered_df)} оригиналов договоров')
+
+
